eb83c6f4658aa5141853fbe6192ebc0b6417e20f
Katiana Stephens <kcjstephens@gmail.com>
2017-11-25 20:13:36 -0500

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule }   from '@angular/router'; 
+import { ROUTES } from './app.routes';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { TrendingComponent } from './trending/trending.component';
+import { SearchComponent } from './search/search.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { GiphyService } from './giphy.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    TrendingComponent,
+    SearchComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule, 
+    HttpModule, 
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [GiphyService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
